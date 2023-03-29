5fdfdc048c3071752f0edcefc76ce9c20d14c85b
sam <samatmldl@gmail.com>
2023-03-29 22:51:48 +0530

    day1

--- recursion/test.py
+++ recursion/test.py
@@ -1,1 +1,17 @@
-print("heloo Niki")
+def factorial(n):
+    if (n==0):
+        return 1;
+    else:
+        return n*factorial(n-1);
+
+def fibonacci(n):
+    if n==0:
+        return 0;
+    elif n==1:
+        return 1;
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+print(factorial(6))
+print(fibonacci(7))
