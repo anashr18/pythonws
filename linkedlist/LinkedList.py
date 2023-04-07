d6df4e5ad1d7dca40f1386f3aee56520e298617b
sam <samatmldl@gmail.com>
2023-04-07 11:11:38 +0530

    LL class

---
+++ linkedlist/LinkedList.py
@@ -0,0 +1,39 @@
+from random import randint
+class Node:
+    def __init__(self, value=None):
+        self.value= value
+        self.next = None
+        self.prev = None
+    def __str__(self):
+        return self.value
+class CustomLL:
+    def __init__(self, values=[]):
+        self.head = None
+        self.tail = None
+        for value in values:
+             self.addLL(value)
+    def addLL(self, value):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+    def __iter__(self):
+        currNode = self.head
+        while currNode:
+            yield currNode
+            currNode = currNode.next
+    def __str__(self):
+        values = [str(x.value) for x in self]
+        return '->'.join(values)
+    def generate(self, n , minVal, maxVal):
+        self.head = None
+        self.tail = None
+        for i in range(n):
+            self.addLL(randint(minVal, maxVal))
+        return self
+
+ll = CustomLL([1,2,3,4,56,7])
+print(ll)
