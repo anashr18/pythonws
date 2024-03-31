2aa1ba2a283d145d8ffe1069cc78e9e24a61e007
anashr18 <anand.shri18@gmail.com>
2024-03-31 02:45:41 +0000

    a test change

--- linkedlist/basic.py
+++ linkedlist/basic.py
@@ -1,58 +1,64 @@
+# a test change
 class Node:
     def __init__(self, value=None):
         self.value = value
         self.next = None
+
+
 class SLL:
     def __init__(self):
         self.head = None
         self.tail = None
+
     def __iter__(self):
         node = self.head
         while node:
             yield node
             node = node.next
+
     def insertSLL(self, loc, value):
         newNode = Node(value)
         if self.head is None:
             self.head = newNode
             self.tail = newNode
         else:
-            if loc==0:
+            if loc == 0:
                 newNode.next = self.head
                 self.head = newNode
-            elif loc==-1:
+            elif loc == -1:
                 self.tail.next = newNode
                 self.tail = newNode
             else:
                 tempNode = self.head
                 index = 0
-                while index < loc-1:
+                while index < loc - 1:
                     tempNode = tempNode.next
-                    index+=1
+                    index += 1
                 newNode.next = tempNode.next
                 tempNode.next = newNode
                 if tempNode == self.tail:
                     self.tail = newNode
+
     def traverseSLL(self):
         if self.head is None:
             print("SLL is empty")
         else:
             node = self.head
             while node is not None:
-                print(str(node.value)+" ")
+                print(str(node.value) + " ")
                 node = node.next
+
     def deleteSLL(self, loc):
-        
-        if loc==0:
-            if self.head ==self.tail:
-                self.head=None
-                self.tail=None
+        if loc == 0:
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
             else:
                 self.head = self.head.next
-        elif loc==-1:
-            if self.head ==self.tail:
-                self.head=None
-                self.tail=None
+        elif loc == -1:
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
             else:
                 node = self.head
                 while node is not None:
@@ -63,17 +69,19 @@ class SLL:
                 self.tail = node
         else:
             tempNode = self.head
-            index=0
-            while index <loc-1:
-                index+=1
+            index = 0
+            while index < loc - 1:
+                index += 1
                 tempNode = tempNode.next
             nextNode = tempNode.next
             tempNode.next = nextNode.next
+
+
 sll = SLL()
-sll.insertSLL(0,100)
-sll.insertSLL(0,80)
-sll.insertSLL(0,50)
-sll.insertSLL(0,10)
+sll.insertSLL(0, 100)
+sll.insertSLL(0, 80)
+sll.insertSLL(0, 50)
+sll.insertSLL(0, 10)
 print([node.value for node in sll])
 sll.traverseSLL()
 sll.deleteSLL(1)
