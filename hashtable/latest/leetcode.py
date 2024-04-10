af184fde039badeae1752bd6b141570151457472
anashr18 <anand.shri18@gmail.com>
2024-04-10 12:46:48 +0000

    added all leet for hashtable

--- hashtable/latest/leetcode.py
+++ hashtable/latest/leetcode.py
@@ -90,5 +90,49 @@ def has_unique_chars(string):
     pass
 
 
-print(has_unique_chars("abcdefg"))  # should return True
-print(has_unique_chars("hello"))  # should return False
+# print(has_unique_chars("abcdefg"))  # should return True
+# print(has_unique_chars("hello"))  # should return False
+
+
+def find_pairs(arr1, arr2, target):
+    result = []
+    arr1_set = set(arr1)
+    for idx, num in enumerate(arr2):
+        diff = target - num
+        if diff in arr1_set:
+            result.append((diff, num))
+    return result
+    pass
+
+
+# arr1 = [1, 2, 3, 4, 5]
+# arr2 = [2, 4, 6, 8, 10]
+# target = 7
+
+# pairs = find_pairs(arr1, arr2, target)
+# print(pairs)
+
+
+def longest_consecutive_sequence(nums):
+    nums_set = set(nums)
+    longest_seq = []
+    longest_streak = 0
+    for idx, num in enumerate(nums):
+        # check if start of sequence
+        if num + 1 not in nums_set:
+            current_seq = []
+            current_seq.append(num)
+            current_streak = 1
+            while num - 1 in nums_set:
+                current_seq.append(num - 1)
+                current_streak += 1
+                num -= 1
+            if current_streak > longest_streak:
+                longest_streak = current_streak
+                longest_seq = current_seq
+    return longest_seq
+    pass
+
+
+print(longest_consecutive_sequence([100, 4, 200, 1, 3, 2]))
+print(longest_consecutive_sequence([99, 201, 200, 202, 203, 205, 204, 96, 97, 98, 100]))
